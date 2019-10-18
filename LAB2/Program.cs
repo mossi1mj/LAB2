6028ffcd2045ce78dc7bdab542016e41678d6bc8
mossi1mj <56240880+mossi1mj@users.noreply.github.com>
2019-10-17 21:22:37 -0400

    first commit

---
+++ LAB2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "y";
+
+            while (answer == "y" || answer != "n")
+            {
+                Console.WriteLine("Welcome to Grand Circus’ Room Detail Generator!");
+
+                double length, width, area, perimeter;
+
+                Console.WriteLine("Enter Lenght: ");
+                length = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Width: ");
+                width = double.Parse(Console.ReadLine());
+
+                perimeter = (length + width) * 2;
+                area = length * width;
+
+                Console.WriteLine($"Area: {Math.Round(area, 2)}");
+                Console.WriteLine($"Perimeter: {Math.Round(perimeter, 2)}");
+                
+                Console.WriteLine("Continue? (y/n)");
+                answer = Console.ReadLine().ToLower();
+
+                if (answer != "n")
+                {
+                    Console.WriteLine($"\nYou have entered {answer}\nPlease type either (y/n).\n");
+                }
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
