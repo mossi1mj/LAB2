66bc94a175e3a504fd80610da5cc241f4f7abc57
mossi1mj <mossjmyron@gmail.com>
2019-10-18 12:42:19 -0400

    second commit

--- LAB2/Program.cs
+++ LAB2/Program.cs
@@ -6,33 +6,44 @@ namespace LAB2
     {
         static void Main(string[] args)
         {
+            bool resume = true;
             string answer = "y";
-
-            while (answer == "y" || answer != "n")
+            Console.WriteLine("Welcome to Grand Circus’ Room Detail Generator!");
+            do
             {
-                Console.WriteLine("Welcome to Grand Circus’ Room Detail Generator!");
-
-                double length, width, area, perimeter;
+                double length, width, hieght, area, perimeter, volume;
 
                 Console.WriteLine("Enter Lenght: ");
                 length = double.Parse(Console.ReadLine());
                 Console.WriteLine("Enter Width: ");
                 width = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Hieght: ");
+                hieght = double.Parse(Console.ReadLine());
 
                 perimeter = (length + width) * 2;
                 area = length * width;
+                volume = length * width * hieght;
 
                 Console.WriteLine($"Area: {Math.Round(area, 2)}");
                 Console.WriteLine($"Perimeter: {Math.Round(perimeter, 2)}");
-                
+                Console.WriteLine($"Volume: {Math.Round(volume, 2)}");
+
                 Console.WriteLine("Continue? (y/n)");
                 answer = Console.ReadLine().ToLower();
 
-                if (answer != "n")
+                while (answer != "y" && answer != "n")
                 {
-                    Console.WriteLine($"\nYou have entered {answer}\nPlease type either (y/n).\n");
+                    Console.WriteLine($"You have enetered {answer}\nPlease enter either (y/n)");
+                    answer = Console.ReadLine().ToLower();
+                }
+                if (answer == "n")
+                {
+                    Console.WriteLine("Good Bye");
+                    resume = false;
                 }
             }
+            while (resume);
+
             Console.ReadLine();
 
 
